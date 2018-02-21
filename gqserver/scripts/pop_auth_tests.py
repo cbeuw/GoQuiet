fc3df836e2856d6d4283764ef01a16a7b4ac1cfe
Qian Wang <cbeuw.andy@gmail.com>
2018-02-21 18:28:28 +0000

    Comments

--- gqserver/scripts/pop_auth_tests.py
+++ gqserver/scripts/pop_auth_tests.py
@@ -4,11 +4,6 @@ import hashlib
 import time
 import random
 
-"""
-DO NOT USE pyCrypto, use pyCryptodome instead.
-The AES CFB mode from pyCrypto is WRONG
-I spent 3 hours trying to figure out why the test case wouldn't pass, until I read this: https://github.com/dlitz/pycrypto/issues/226
-"""
 
 TIME_HINT = 3600
 KEY = "testkey"
@@ -23,6 +18,7 @@ with open('../tests/auth/.base','rb') as f:
     print("aes_key: " + aes_key.hex())
     cipher = AES.new(aes_key,AES.MODE_CFB,iv,segment_size=128)
     # segment_size has to be 128 because it's default to 8 in pycryptodome, but 128 in golang crypto/aes
+    # ^it took me 3 hours to realise where I went wrong.
     goal = hashlib.sha256((str(t//TIME_HINT)+KEY).encode()).digest()[0:16]
     print("goal: " + goal.hex())
     out = cipher.encrypt(goal)
